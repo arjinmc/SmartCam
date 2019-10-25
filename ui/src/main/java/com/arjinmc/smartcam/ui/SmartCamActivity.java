1c644ffebbe77166b718d80de7a76243d136339d
arjinmc <arjinmc@hotmail.com>
2019-10-25 19:23:53 +0800

    add resources

--- ui/src/main/java/com/arjinmc/smartcam/ui/SmartCamActivity.java
+++ ui/src/main/java/com/arjinmc/smartcam/ui/SmartCamActivity.java
@@ -3,6 +3,8 @@ package com.arjinmc.smartcam.ui;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
@@ -24,6 +26,10 @@ public class SmartCamActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.smartcam_act_main);
 
         mSmartCamPreview = findViewById(R.id.smartcam_preview);
