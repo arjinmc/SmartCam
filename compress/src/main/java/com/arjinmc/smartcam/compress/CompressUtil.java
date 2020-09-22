40440003fc04dad65e6ec75403da862086fdb02d
arjinmc <arjinmc@hotmail.com>
2020-09-22 15:40:59 +0800

    optimize codes

---
+++ compress/src/main/java/com/arjinmc/smartcam/compress/CompressUtil.java
@@ -0,0 +1,11 @@
+package com.arjinmc.smartcam.compress;
+
+/**
+ * CompressUtil
+ * Created by Eminem Lo on 8/9/2020.
+ * email: arjinmc@hotmail.com
+ */
+public final class CompressUtil {
+
+//    public static native void compressQuantiy(File file);
+}
--- transfer/src/main/cpp/Crop.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by arjinmc on 11/8/2020.
-//
-
-#ifndef SMARTCAM_CROP_H
-#define SMARTCAM_CROP_H
-
-#endif //SMARTCAM_CROP_H
