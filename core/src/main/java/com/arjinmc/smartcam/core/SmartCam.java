97013f356cf8d2f44c20ac75a680b24685b977ce
arjinmc <arjinmc@hotmail.com>
2020-10-23 18:15:52 +0800

    revert code

--- core/src/main/java/com/arjinmc/smartcam/core/SmartCam.java
+++ core/src/main/java/com/arjinmc/smartcam/core/SmartCam.java
@@ -39,14 +39,14 @@ public class SmartCam extends AbsCameraWrapper {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             mCameraWrapper = new Camera2Wrapper(getContext());
             mCameraVersion = CameraVersion.VERSION_2;
-//            try {
-//                if (!((Camera2Wrapper) mCameraWrapper).isUseCamera2Better()) {
-//                    mCameraWrapper = new Camera1Wrapper();
-//                    mCameraVersion = CameraVersion.VERSION_1;
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
+            try {
+                if (!((Camera2Wrapper) mCameraWrapper).isUseCamera2Better()) {
+                    mCameraWrapper = new Camera1Wrapper();
+                    mCameraVersion = CameraVersion.VERSION_1;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else {
             mCameraWrapper = new Camera1Wrapper();
             mCameraVersion = CameraVersion.VERSION_1;
